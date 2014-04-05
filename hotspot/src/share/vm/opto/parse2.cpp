a8deb7dae88a5bc7415cd7a2ec8640fec80ec0af
ravitandon <ravifreek63@gmail.com>
2014-04-05 18:01:02 -0400

    added code to increment access counter

--- hotspot/src/share/vm/opto/parse2.cpp
+++ hotspot/src/share/vm/opto/parse2.cpp
@@ -69,8 +69,8 @@ void Parse::array_store(BasicType elem_type) {
 void Parse::increment_access_counter(BasicType elem_type){
   Node *obj = peek(0); // Getting the address of the object from the stack
   Node *counter = load_object_counter(obj); // Gets the object counter from the header of the object
-  Node *incr_node = _gvn.transform(new (C, 3) AddINode(counter, _gvn.intcon(1))); // incrementing the counter variable by 1
-  const TypePtr* adr_type = Type::get_const_basic_type(elem_type); // Getting the basic type pointer
+  Node *incr_node = _gvn.transform(new (C, 3) AddINode(counter, _gvn.intcon(1))); // incrementing the counter variable by 1, do not understand
+  const TypePtr* adr_type = new TypePtr(Type::Int, TypePtr::NotNull, 0);
   store_to_memory(control(), counter, incr_node, elem_type, adr_type); // Storing the result obtained after the increment operation to memory
 }
 
