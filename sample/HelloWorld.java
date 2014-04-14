715023ee3849e0cf5925ee314dab6c34e52d85ed
ravitandon <ravifreek63@gmail.com>
2014-04-13 22:04:15 -0400

    added sample folder

---
+++ sample/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public int member;
+    public static void main(String[] args) {
+    HelloWorld h = new HelloWorld();
+    System.out.println("Hello, World");
+    System.gc();
+    }
+    
+}
+
