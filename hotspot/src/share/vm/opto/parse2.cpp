3d6d74a584fd0129a34ab520b2dce2a9b10a806f
ravitandon <ravifreek63@gmail.com>
2014-04-21 02:20:36 -0400

    control null for increment access counter

--- hotspot/src/share/vm/opto/parse2.cpp
+++ hotspot/src/share/vm/opto/parse2.cpp
@@ -1190,14 +1190,13 @@ void Parse:: increment_access_counter(Node *obj){
 	set_control(null_true);
 }
 
-
 void Parse::increment_count(Node *obj){
   int adr_type = Compile::AliasIdxRaw;
-  Node *counter_addr = basic_plus_adr(obj, oopDesc::counter_offset_in_bytes());
+  Node *counter_addr = basic_plus_adr(obj, 0);
   Node* ctrl = control();
   Node* count  = make_load(ctrl, counter_addr, TypeLong::LONG, T_LONG, adr_type);
   Node *incr_node = _gvn.transform(new (C, 3) AddLNode(count, _gvn.longcon(1))); // incrementing the counter variable by 1, do not understand
-  store_to_memory(ctrl, counter_addr, incr_node, T_LONG, adr_type); // Storing the result obtained after the increment operation to memory
+  store_to_memory(NULL, counter_addr, incr_node, T_LONG, adr_type); // Storing the result obtained after the increment operation to memory
 }
 
 //----------------------------adjust_map_after_if------------------------------
