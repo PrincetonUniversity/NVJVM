4b4f4faa40e11bcd61725d6219b666585f447873
ravitandon <ravifreek63@gmail.com>
2014-04-28 03:37:44 -0400

    fixed bug

--- hotspot/src/cpu/x86/vm/templateTable_x86_64.cpp
+++ hotspot/src/cpu/x86/vm/templateTable_x86_64.cpp
@@ -571,6 +571,7 @@ void TemplateTable::dload() {
 void TemplateTable::aload() {
   transition(vtos, atos);
   locals_index(rbx);
+  Register temp = new Register();
   Label nullObj;
   int ce_offset =  oopDesc::counter_offset_in_bytes();
   __ movptr(rax, aaddress(rbx));
@@ -580,9 +581,9 @@ void TemplateTable::aload() {
   __ movptr(rax, object);
   __ testptr(rax, rax);
   __ jcc(Assembler::zero, nullObj);
-  __ movl(rax, object);        // load access counter
-  __ incrementl(rax, 1);       // increment access counter
-  __ movl(object, rax);        // store access counter
+  __ movl(temp, object);        // load access counter
+  __ incrementl(temp, 1);       // increment access counter
+  __ movl(object, temp);        // store access counter
   __ bind(nullObj);
   __ movptr(rax, aaddress(rbx));
 }
@@ -774,15 +775,16 @@ void TemplateTable::aload(int n) {
   Label nullObj;
   int ce_offset =  oopDesc::counter_offset_in_bytes();
   __ movptr(rax, aaddress(n));
+  Register temp = new Register();
   Address object = Address(rax, ce_offset);
   __ testptr(rax, rax);
   __ jcc(Assembler::zero, nullObj);
   __ movptr(rax, object);
   __ testptr(rax, rax);
   __ jcc(Assembler::zero, nullObj);
-  __ movl(rax, object);        // load access counter
-  __ incrementl(rax, 1);       // increment access counter
-  __ movl(object, rax);        // store access counter
+  __ movl(temp, object);        // load access counter
+  __ incrementl(temp, 1);       // increment access counter
+  __ movl(object, temp);        // store access counter
   __ bind(nullObj);
   __ movptr(rax, aaddress(n));
 }
