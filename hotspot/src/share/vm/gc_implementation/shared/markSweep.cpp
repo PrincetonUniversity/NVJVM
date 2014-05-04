6920ad7b416e73b49e57abdecc5bcafea4426967
ravitandon <ravifreek63@gmail.com>
2014-05-04 02:07:41 -0400

    a

--- hotspot/src/share/vm/gc_implementation/shared/markSweep.cpp
+++ hotspot/src/share/vm/gc_implementation/shared/markSweep.cpp
@@ -120,7 +120,8 @@ void MarkSweep::follow_stack() {
     while (!_marking_stack.is_empty()) {
       oop obj = _marking_stack.pop();
       assert (obj->is_gc_marked(), "p must be marked");
-      if(obj->getCount() != 0) printf("access count = %" PRId64 "\n", obj->getCount());
+      if(obj->getCount() != 0) printf("access count = %" PRIu64 ", hash = %d, size =%zd\n", obj->getCount(), obj->mark()->hash(),
+    		  obj->mark()->size());
       obj->follow_contents();
     }
     // Process ObjArrays one at a time to avoid marking stack bloat.
