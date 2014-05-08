0941b001a722f67fb63b90cb390da60d88392689
ravitandon <ravifreek63@gmail.com>
2014-05-08 05:05:37 -0400

    a

--- hotspot/src/share/vm/gc_implementation/g1/g1CollectedHeap.cpp
+++ hotspot/src/share/vm/gc_implementation/g1/g1CollectedHeap.cpp
@@ -4417,7 +4417,8 @@ oop G1ParCopyHelper::copy_to_survivor_space(oop old) {
   Prefetch::write(obj_ptr, PrefetchCopyIntervalInBytes);
   oop forward_ptr = old->forward_to_atomic(obj);
   if (L_DEBUG){
-	  printf("In copy_to_survivor_space, obj=%p, count=%p, fwd_ptr=%p\n", obj, ((oop)obj)->getCount(), forward_ptr); fflush(stdout);
+	  printf("In copy_to_survivor_space, obj=%p, count=%p, fwd_ptr=%p, heap region is young %d,\n", obj, ((oop)obj)->getCount(), forward_ptr,
+			  from_region->is_young() ); fflush(stdout);
   }
   if (forward_ptr == NULL) {
     Copy::aligned_disjoint_words((HeapWord*) old, obj_ptr, word_sz);
--- hotspot/src/share/vm/gc_implementation/shared/markSweep.inline.hpp
+++ hotspot/src/share/vm/gc_implementation/shared/markSweep.inline.hpp
@@ -103,7 +103,7 @@ template <class T> inline void MarkSweep::adjust_pointer(T* p, bool isroot) {
     if (L_DEBUG){
     	printf("In adjust pointer, new_obj = %p, old_obj = %p, old obj count =%d\n", new_obj, obj, obj->getCount()); fflush(stdout);
     }
-    new_obj->setCount(obj->getCount());
+
     assert(new_obj != NULL ||                         // is forwarding ptr?
            obj->mark() == markOopDesc::prototype() || // not gc marked?
            (UseBiasedLocking && obj->mark()->has_bias_pattern()) ||
@@ -114,9 +114,10 @@ template <class T> inline void MarkSweep::adjust_pointer(T* p, bool isroot) {
       assert(Universe::heap()->is_in_reserved(new_obj),
              "should be in object space");
       oopDesc::encode_store_heap_oop_not_null(p, new_obj);
+      new_obj->setCount(obj->getCount());
     }
   }
-  assert(new_obj->getCount() == obj->getCount(), "count mismatch in adjust pointer");
+  assert((new_obj == NULL) || (new_obj->getCount() == obj->getCount()), "count mismatch in adjust pointer");
   VALIDATE_MARK_SWEEP_ONLY(track_adjusted_pointer(p, isroot));
 }
 
