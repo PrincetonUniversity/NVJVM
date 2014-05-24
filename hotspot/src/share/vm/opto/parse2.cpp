38309d84e6875320e838a625b95f0f9320ddc513
ravitandon <ravifreek63@gmail.com>
2014-05-24 18:20:42 -0400

    debug

--- hotspot/src/share/vm/opto/parse2.cpp
+++ hotspot/src/share/vm/opto/parse2.cpp
@@ -1187,9 +1187,9 @@ void Parse:: increment_access_counter(Node *obj){
 	Node *chk = _gvn.transform(new (C, 3) CmpPNode(obj, null())); // generate instructions for comparing the object with a null object
 	BoolTest::mask btest = BoolTest::ne;
 	Node *tst = _gvn.transform(new (C, 2) BoolNode(chk, btest));
+	IfNode* iff = create_and_map_if(control(), tst, PROB_LIKELY_MAG(3), COUNT_UNKNOWN);// True branch, use existing map info
   {
 	PreserveJVMState pjvms(this);
-    IfNode* iff = create_and_map_if(control(), tst, PROB_LIKELY_MAG(3), COUNT_UNKNOWN);// True branch, use existing map info
     Node *iftrue  = _gvn.transform( new (C, 1) IfTrueNode (iff) );
     set_control(iftrue);
     r->init_req(edges-1, iftrue);
@@ -1200,7 +1200,7 @@ void Parse:: increment_access_counter(Node *obj){
     set_control(r);
   }
   set_control(r);
-  increment_count(obj, control());
+  //increment_count(obj, control());
 }
 
 void Parse::increment_count(Node *obj, Node *ctrl){
