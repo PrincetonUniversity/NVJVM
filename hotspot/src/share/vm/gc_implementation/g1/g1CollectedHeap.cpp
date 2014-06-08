5954dc74445f9ad45de71471f600645b25ca70e8
ravitandon <ravifreek63@gmail.com>
2014-06-08 18:57:58 -0400

    debuggin region initialization

--- hotspot/src/share/vm/gc_implementation/g1/g1CollectedHeap.cpp
+++ hotspot/src/share/vm/gc_implementation/g1/g1CollectedHeap.cpp
@@ -3702,7 +3702,7 @@ G1CollectedHeap::do_collection_pause_at_safepoint(double target_pause_time_ms) {
 #endif // YOUNG_LIST_VERBOSE
 
       g1_policy()->choose_collection_set(target_pause_time_ms);
-      printf("Number of used regions = %d\n Number of collection regions =%d\n", g1_policy()->used_regions(), g1_policy()->collection_set_size());
+      printf("Number of used regions = %d\n Number of collection regions =%d\n", used_regions(), g1_policy()->collection_set_size());
       fflush(stdout);
 
       // We have chosen the complete collection set. If marking is
