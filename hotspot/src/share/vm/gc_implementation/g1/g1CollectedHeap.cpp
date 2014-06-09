a47bfb9da155bac481f66c4cf5accf7ce9c69ef0
ravitandon <ravifreek63@gmail.com>
2014-06-08 23:39:58 -0400

    fix

--- hotspot/src/share/vm/gc_implementation/g1/g1CollectedHeap.cpp
+++ hotspot/src/share/vm/gc_implementation/g1/g1CollectedHeap.cpp
@@ -745,9 +745,9 @@ HeapRegion* G1CollectedHeap::new_gc_alloc_region(int purpose,
   if (_gc_alloc_region_counts[purpose] < g1_policy()->max_regions(purpose)) {
     alloc_region = new_region(word_size, true /* do_expand */);
 //	  alloc_region = new_region_hybrid(word_size, true, isCold); // allocating region from the hybrid region allocator
-	  if(isCold != alloc_region->get_cold()){
-		  printf("Bug in new_gc_allocation_region code\n."); fflush(stdout); exit(1);
-	  }
+//	  if(isCold != alloc_region->get_cold()){
+//		  printf("Bug in new_gc_allocation_region code\n."); fflush(stdout); exit(1);
+//	  }
     // Change, GCAllocForSurvivedCold is now set as a survivor space
     if ((purpose == GCAllocForSurvived || purpose == GCAllocForSurvivedCold) && alloc_region != NULL) {
       alloc_region->set_survivor();
