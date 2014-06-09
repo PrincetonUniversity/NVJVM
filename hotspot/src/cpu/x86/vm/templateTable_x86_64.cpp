9d5aefd6da471dd3b2ee7240c2277e2c6d80035e
ravitandon <ravifreek63@gmail.com>
2014-06-09 16:41:22 -0400

    optimizing the interpreter flow

--- hotspot/src/cpu/x86/vm/templateTable_x86_64.cpp
+++ hotspot/src/cpu/x86/vm/templateTable_x86_64.cpp
@@ -590,35 +590,37 @@ void TemplateTable::interceptObject(Address object) {
   __ push(r10);
   __ push(r11);
 
-  Label nullObj, hotObject;
+  Label nullObj, coldObject, coldTest1;
 
   __ cmpptr(object, 0);
   __ jcc(Assembler::equal, nullObj);
 
   __ cmpptr(object, coldRegionStart);
-  __ jcc(Assembler::less, hotObject);
+  __ jcc(Assembler::greater, coldTest1);
 
-//  __ cmpptr(object, coldRegionEnd);
-//  __ jcc(Assembler::greater, hotObject);
+   __ movptr(r10, object);
+      Address objectCounter = Address(r10, ce_offset);
+   __ movl(r11, objectCounter);        // load access counter
+   __ incrementl(r11, 1);       		  // increment access counter
+   __ movl(objectCounter, r11);        // store access counter
+   __ jmp(nullObj);
 
+  __ bind(coldTest1);
+  __ cmpptr(object, coldRegionEnd);
+  __ jcc(Assembler::less, coldObject);
+  __ jmp(nullObj);
+
+  __ bind(coldObject);
   __ movptr(r11, object); 	  // pointer to the object in memory
   __ subl(r11, offset);		  // offset of the region, got by subtracting
   __ shrl(r11, REGION_SHIFT); // shifting the register by 20 bits - getting the pointer to region
   __ movptr(r10, (intptr_t)base);
   __ addptr(r11, r10);		  // adding the offset to get the address of the location within memory for the
   __ cmpl(Address(r11, 0), 0);
-  __ jcc(Assembler::equal, hotObject); // moving the value at the byte into the register r10
+  __ jcc(Assembler::equal, nullObj); // moving the value at the byte into the register r10
 
   __ movptr(r10, object);
   call_VM(noreg, CAST_FROM_FN_PTR(address, InterpreterRuntime::_checkObj), r10, r11);
-
-  __ bind(hotObject); 				  // binding hot object to increment the access count
-
-  __ movptr(r10, object);
-     Address objectCounter = Address(r10, ce_offset);
-  __ movl(r11, objectCounter);        // load access counter
-  __ incrementl(r11, 1);       		  // increment access counter
-  __ movl(objectCounter, r11);        // store access counter
 
   __ bind(nullObj);					  // binding the null label here
 
