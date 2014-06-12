5920bcb0ffb4094ccd3f89abcfba6498a5ad23a2
ravitandon <ravifreek63@gmail.com>
2014-06-12 18:26:12 -0400

    adding page protection for debugging

--- hotspot/src/share/vm/swap/PageBuffer.cpp
+++ hotspot/src/share/vm/swap/PageBuffer.cpp
@@ -22,10 +22,12 @@ SSDRange PageBuffer::pageOut(void *va, int np, int off) {
 		printf("In pageOut, paging out %d, top %p\n", np, va); fflush(stdout);
 	}
 	// Write protecting the memory region - only a single thread must have control over the region
-	/*if (mprotect (va, np*PAGE_SIZE, PROT_READ) == -1){
+	if (mprotect (va, np*PAGE_SIZE, PROT_READ) == -1){
 		perror("error :");
-		printf("Error In Write Protecting Page %p \n", va); fflush(stdout);
-	}*/
+		printf("Error In Write Protecting Page %p \n", va);
+		fflush(stdout);
+		exit(1);
+	}
 	SSDRange ssdRange = SwapWriter::swapOut (va, np, off);
 	if (L_SWAP){
 		printf("In pageOut, ssdRange %d, %d\n", ssdRange.getStart(), ssdRange.getEnd()); fflush(stdout);
@@ -38,7 +40,9 @@ SSDRange PageBuffer::pageOut(void *va, int np, int off) {
 	// Marking the region as not needed so that the OS can free the resources
 	if (madvise (va, (unsigned long)(np * _PAGE_SIZE), MADV_DONTNEED) == -1){ // After swap out the page is advised to be not needed
 		perror("error :");
-		printf("Error In Protecting Page %p \n", va); fflush(stdout);
+		printf("Error In Protecting Page %p \n", va);
+		fflush(stdout);
+		exit(1);
 	}
 	return ssdRange;
 }
