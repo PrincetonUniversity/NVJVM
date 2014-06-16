8b1f889042b47fff328ebfc9d439751fcaa00d47
ravitandon <ravifreek63@gmail.com>
2014-06-16 00:58:28 -0400

    conversion error

--- hotspot/src/share/vm/swap/SSDSwap.cpp
+++ hotspot/src/share/vm/swap/SSDSwap.cpp
@@ -32,7 +32,7 @@ void SSDSwap::swapInRegion(void *addr) {
 	for(int count = 0; count < Universe::_regionPages; count++){
 		value = *startRegionTable;
 		if(value == Universe::_notPresentMask || value == Universe::_partiallyFilledMask ){
-			SwapManager::remapPage(regionPos);
+			SwapManager::remapPage((void *)regionPos);
 		} else if(value != Universe::_presentMask){
 			printf("Error, value in the region table different from 0,1,2. Exiting.\n");
 			fflush(stdout);
@@ -40,7 +40,7 @@ void SSDSwap::swapInRegion(void *addr) {
 		}
 		startRegionTable++;
 		prefetchPosition++;
-		regionPos = (void*) ((char *)regionPos + _PAGE_SIZE);
+		regionPos = regionPos + _PAGE_SIZE;
 	}
 }
 
