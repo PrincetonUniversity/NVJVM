5fc3ae66a81931c2cdf1d7f1472b143cc37b157f
ravitandon <ravifreek63@gmail.com>
2014-07-02 17:31:49 -0400

    order regions change

--- hotspot/src/share/vm/gc_implementation/g1/collectionSetChooser.cpp
+++ hotspot/src/share/vm/gc_implementation/g1/collectionSetChooser.cpp
@@ -272,7 +272,8 @@ CollectionSetChooser::sortMarkedHeapRegions() {
            "Or we didn't reserved enough length");
     _markedRegions.trunc_to(_first_par_unreserved_idx);
   }
-  _markedRegions.sort(orderRegionsMemPressure);
+  _markedRegions.sort(orderRegions);
+  //  _markedRegions.sort(orderRegionsMemPressure);
   assert(_numMarkedRegions <= _markedRegions.length(), "Requirement");
   assert(_numMarkedRegions == 0
          || _markedRegions.at(_numMarkedRegions-1) != NULL,
