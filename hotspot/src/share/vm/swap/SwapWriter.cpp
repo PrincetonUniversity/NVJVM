49f2b2c6e4b7f0f98bc51e659960eeb6dd321cfe
ravitandon <ravifreek63@gmail.com>
2014-07-04 18:34:16 -0400

    debugging

--- hotspot/src/share/vm/swap/SwapWriter.cpp
+++ hotspot/src/share/vm/swap/SwapWriter.cpp
@@ -17,6 +17,26 @@ SwapWriter::~SwapWriter() {
 	// TODO Auto-generated destructor stub
 }
 
+void check(){
+	char file[] = "/home/tandon/swap.txt";
+	FILE *f = fopen(file, "r");
+	fseek(f, (long)(0), SEEK_SET);
+	char va ;
+	int count, sum = 0;
+	for (count = 0; count < 4096 * 256; count++){
+		fread(&va, sizeof(char),  1, f);
+		if(va){
+			printf("%d,", va);
+			sum++;
+		}
+	}
+	fclose(f);
+	if(sum == 0){
+		printf("All zeros. Something is wrong.\n");
+	}
+	exit(-1);
+}
+
 // Writes a set number of pages to the offset in the file, assumes the page to be unprotected.
 // Page needs to be protected later on.
 SSDRange SwapWriter::swapOut (void * va, int np, int off){
@@ -39,14 +59,16 @@ SSDRange SwapWriter::swapOut (void * va, int np, int off){
 	  // What happens when another thread writes on the address space when that part of the address space is being swapped out ?
 	  size_t len = fwrite(va, sizeof(char), (long)(np * _PAGE_SIZE), f);
 	  if (len == 0){
-		  fputs ("Error writing swap file\n", stderr); fflush(stdout);
+		  fputs ("Error writing swap file\n", stderr);
+		  fflush(stdout);
 	  } else {
 		  if(L_SWAP){
-			  printf("Written %zd bytes to the file\n", len); fflush(stdout);
+			  printf("Written %zd bytes to the file\n", len);
+			  fflush(stdout);
 		  }
 	  }
 	  fclose (f);
-
+	  check();
 	  SwapMetric::incrementSwapOutsV(np);
 	  SwapMetric::incrementSwapOutBytes(np*_PAGE_SIZE);
 
