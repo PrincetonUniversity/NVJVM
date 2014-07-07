fa09f4c7a05e9a5777ea9905a91ce64a1680dfb9
ravitandon <ravifreek63@gmail.com>
2014-07-07 16:20:24 -0400

    debugging

--- hotspot/src/share/vm/swap/SwapManager.cpp
+++ hotspot/src/share/vm/swap/SwapManager.cpp
@@ -63,9 +63,11 @@ void SwapManager::remapPage(void *address, bool partialCheck = true){
 	  return;
   }
   if(Universe::isPresent(address)){
-	  printf("Address %p, index = %d is already present. "
+	  if(L_SWAP){
+		  printf("Address %p, index = %d is already present. "
 			  "Therefore, page not fetched in.\n", address, Universe::getPageIndex(address));
 	  fflush(stdout);
+	  }
 	  return;
   }
 
