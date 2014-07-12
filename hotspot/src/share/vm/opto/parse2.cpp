f4a6295e14981d634587cfcb40755384ebc42c21
ravitandon <ravifreek63@gmail.com>
2014-07-12 00:50:41 -0400

    debugging seg faults

--- hotspot/src/share/vm/opto/parse2.cpp
+++ hotspot/src/share/vm/opto/parse2.cpp
@@ -72,7 +72,7 @@ void Parse::array_store(BasicType elem_type) {
 Node* Parse::array_addressing(BasicType type, int vals, const Type* *result2) {
   Node *idx   = peek(0+vals);   // Get from stack without popping
   Node *ary   = peek(1+vals);   // in case of exception
-
+  increment_access_counter(peek(1+vals));
   // Null check the array base, with correct stack contents
   ary = do_null_check(ary, T_ARRAY);
   // Compile-time detect of null-exception?
@@ -1658,7 +1658,7 @@ void Parse::do_one_bytecode() {
   case Bytecodes::_saload: array_load(T_SHORT);  break;
   case Bytecodes::_faload: array_load(T_FLOAT);  break;
   case Bytecodes::_aaload: {
-	  increment_access_counter(peek(1));
+//	  increment_access_counter(peek(1));
 	  array_load(T_OBJECT);
 	  break;
   }
