c936e6ce9da9adb0e620d648c00a8dc402bdfe1b
ravitandon <ravifreek63@gmail.com>
2014-08-27 17:00:17 -0400

    removed a printf statement

--- hotspot/src/share/vm/memory/space.hpp
+++ hotspot/src/share/vm/memory/space.hpp
@@ -79,7 +79,7 @@ class DirtyCardToOopClosure;
 class SwapInOopClosure: public OopClosure {
 protected:
  template <class T> void do_oop_work(T* p) {
-	 printf("In SwapInClosure, do_oop_work.\n");
+//	 printf("In SwapInClosure, do_oop_work.\n");
 	 	T heap_oop = oopDesc::load_heap_oop(p);
 	    if (!oopDesc::is_null(heap_oop)) {
 	      oop obj = oopDesc::decode_heap_oop_not_null(heap_oop);
