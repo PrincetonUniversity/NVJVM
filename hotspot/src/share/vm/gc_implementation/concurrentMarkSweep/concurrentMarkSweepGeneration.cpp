4a284bac96dbc7ef5c6e6f8635caec5168753a5c
ravitandon <ravifreek63@gmail.com>
2014-09-02 16:50:49 -0400

    added assertions in OCMS

--- hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
+++ hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
@@ -3938,7 +3938,8 @@ void CMSConcMarkingTask::work(int i) {
   __check(_chunkList->listSize() == 0, "After do_scan_and_mark. ChunkListSize NonZero.");
   __check(_collector->compareBitMaps() == 0, "After do_scan_and_mark. Comparison between mark and grey bitmap not 1.");
   __check(_collector->compareDummyBitMaps() == 1, "Dummy bitmap comparison.");
-  printf("Comparing with the dummy bitmap = %d.\n", _collector->compareDummyBitMaps());
+  __check(Universe::totalGreyObjectCount() == 0, "total grey object count non zero after CMS Concurrent Mark.");
+  __check(_collector->_greyMarkBitMap.isAllClear(), "grey bit map must be all clear after CMS Concurrent Mark.");
 #endif
 
   _timer.stop();
