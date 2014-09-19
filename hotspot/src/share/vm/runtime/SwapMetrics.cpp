e220c1e9f2ff4dce53e0660274502ce40c463a17
ravitandon <ravifreek63@gmail.com>
2014-09-19 18:49:34 -0400

    added files for tracking the swap metrics

---
+++ hotspot/src/share/vm/runtime/SwapMetrics.cpp
@@ -0,0 +1,59 @@
+/*
+ * SwapMetrics.cpp
+ *
+ *  Created on: Sep 19, 2014
+ *      Author: ravitandon
+ */
+
+#include "SwapMetrics.hpp"
+
+SwapMetrics::SwapMetrics(const char* phase) {
+  _phaseName = std::string(phase);
+  int faults[] = getCurrentNumberOfFaults();
+  int count;
+  for (count = 0; count < 2; count++){
+	  _initialFaults[count] = faults[count];
+  }
+  _logFilePath = "/home/tandon/logs/cms.log";
+}
+
+SwapMetrics::~SwapMetrics() {
+  int faults[] = getCurrentNumberOfFaults();
+  int count;
+  for (count = 0; count < 2; count++){
+	  _finalFaults[count] = faults[count];
+  }
+  // Setting the minor and the major faults
+  _minorFaults = _finalFaults[0] - _initialFaults[0];
+  _majorFaults = _finalFaults[1] - _initialFaults[1];
+
+  // Writing the minor and the major faults to the output
+  cout  << _phaseName << "," << _minorFaults << "," << _majorFaults << endl;
+}
+
+std::string inToS(int num){
+    std::ostringstream ss(num);
+    return ss.str();
+}
+
+int* SwapMetrics::getCurrentNumberOfFaults(void){
+	  int count = 0;
+	  int faults[] = {0 , 0};
+	  FILE *fp;
+	  char buf[BUF_MAX];
+	  pid_t pid = getpid();
+	  std::string cmd = std::string("ps -o min_flt,maj_flt ") +
+	           std::string(inToS(pid));
+	  fp = popen(cmd.c_str(), "r");
+	  while(fgets(buf, BUF_MAX, fp) != NULL);
+	  istringstream iss(buf);
+	  do
+	   {
+	       string sub;
+	       iss >> sub;
+	       std::stringstream(sub) >> faults[count];
+	       count++;
+	    } while(iss);
+
+	  return faults;
+}
