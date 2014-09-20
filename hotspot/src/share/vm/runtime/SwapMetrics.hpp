4dd37714f00a2b65009b43ee58cb9cb62818ca93
Ravi Tandon <ravifreek63@gmail.com>
2014-09-19 21:21:07 -0400

    Create SwapMetrics.hpp

---
+++ hotspot/src/share/vm/runtime/SwapMetrics.hpp
@@ -0,0 +1,39 @@
+/*
+ * SwapMetrics.hpp
+ *
+ *  Created on: Sep 19, 2014
+ *      Author: ravitandon
+ */
+
+#ifndef SWAPMETRICS_HPP_
+#define SWAPMETRICS_HPP_
+#include <stdio.h>
+#include <sstream>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
+using namespace std;
+#define BUF_MAX 50
+
+
+class SwapMetrics {
+private:
+	int _majorFaults;
+	int _minorFaults;
+    int* _initialFaults;
+    int* _finalFaults;
+    string _phaseName;
+    string _logFilePath;
+    int* _currentFaults;
+
+public:
+	SwapMetrics(const char *phase);
+	virtual ~SwapMetrics();
+	int getMajorFaultMetrics() { return _majorFaults; }
+	int getMinorFaultsMetrics() { return _minorFaults; }
+	void getCurrentNumberOfFaults();
+};
+
+#endif /* SWAPMETRICS_HPP_ */
