e092e3131e49e2f62465492cb7b950a54c248ab8
ravitandon <ravifreek63@gmail.com>
2014-09-21 19:49:59 -0400

    removed logs

--- hotspot/src/share/vm/memory/universe.hpp
+++ hotspot/src/share/vm/memory/universe.hpp
@@ -42,13 +42,13 @@
 #define OCMS_ASSERT 0
 #define OCMS_NO_GREY_LOG 0
 #define OCMS_NO_GREY_LOG_HIGH 0
-#define OCMS_NO_GREY_ASSERT 1
+#define OCMS_NO_GREY_ASSERT 0
 #define OCMS_NO_GREY_DEBUG 0
 #define OCMS_METRICS 0
 #define PRINT_TO_LOG 0
 #define TRACING_SYSTEM_GC 0
-#define OC_SWEEP_ASSERT 1
-#define OC_SWEEP_LOG 1
+#define OC_SWEEP_ASSERT 0
+#define OC_SWEEP_LOG 0
 
 
 // in microseconds, the overall time the worker threads sleep for after they have been set to wait
