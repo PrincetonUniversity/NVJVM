e68a2e8d96edb135329374bc643b816549cdc9e7
ravitandon <ravifreek63@gmail.com>
2014-10-31 15:44:01 -0400

    turned on the knob for incrementing sweep

--- hotspot/src/share/vm/memory/universe.hpp
+++ hotspot/src/share/vm/memory/universe.hpp
@@ -56,7 +56,7 @@
 #define SWEEP_PARTITIONED 1
 #define SWEEP_REGULAR (SWEEP_PARTITIONED == 0)
 #define SWEEP_THRESHOLD 0.75
-#define INC_SWEEP 0
+#define INC_SWEEP 1
 
 // in microseconds, the overall time the worker threads sleep for after they have been set to wait
 #define WORKER_THREAD_SLEEP_TIME 100
