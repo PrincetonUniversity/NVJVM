78364cf28b148d06662a79d4531bda7fac46b8b3
ravitandon <ravifreek63@gmail.com>
2014-11-09 23:46:54 -0500

    added asynch writes

--- hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
+++ hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
@@ -4268,7 +4268,7 @@ void CMSConcSweepingTask::do_partition(int currentPartitionIndex, SweepPageClosu
 		pageCount++;
 		if(pageCount>inCoreCount){
 			if(msync(_partitionMetaData->getPageBase(pageIndex),
-					_PAGE_SIZE, MS_INVALIDATE) == -1){
+					_PAGE_SIZE, MS_ASYNC) == -1){
 				printf("Error in msync.\n");
 				perror("Error:");
 				exit (-1);
@@ -4564,7 +4564,7 @@ void CMSConcMarkingTask::do_scan_and_mark_OCMS_NO_GREY_BATCHED(int i){
 				pagesScanned++;
 				if(pagesScanned>inCoreCount){
 					if(msync(_partitionMetaData->getPageBase(pageIndex),
-							_PAGE_SIZE, MS_INVALIDATE) == -1){
+							_PAGE_SIZE, MS_ASYNC) == -1){
 						printf("Error in msync.\n");
 						perror("Error:");
 						exit (-1);
