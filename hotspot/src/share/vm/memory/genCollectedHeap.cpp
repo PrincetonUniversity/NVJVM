e7c5a619098df0c459a5bfc1e4820adbd9bceb65
ravitandon <ravifreek63@gmail.com>
2014-11-16 18:15:11 -0500

    tracing system.gc

--- hotspot/src/share/vm/memory/genCollectedHeap.cpp
+++ hotspot/src/share/vm/memory/genCollectedHeap.cpp
@@ -833,7 +833,11 @@ void GenCollectedHeap::collect(GCCause::Cause cause) {
 #else
     // Stop-the-world full collection
 	 cout << "should_do_concurrent_full_gc(cause) false, calling stw :: collect " << endl;
-    collect(cause, n_gens() - 1);
+     //collect(cause, n_gens() - 1);
+
+	ConcurrentMarkSweepGeneration* cmsCMSG = _gens[1];
+	cmsCMSG->collector()->collect_in_background(false);
+
 #endif
   }
 }
