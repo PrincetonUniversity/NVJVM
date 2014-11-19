b7717ea49d830ac5a03698a2192f599ce0b36fca
ravitandon <ravifreek63@gmail.com>
2014-11-18 23:08:45 -0500

    added code for dynamic adjustment

--- hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
+++ hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
@@ -7403,8 +7403,10 @@ void Par_MarkFromRootsClosure::scan_oops_in_oop(HeapWord* ptr) {
   assert(res, "Will hold once we use workqueues");
   while (true) {
     oop new_oop;
-    	while(SwapMetrics::_overutilized){
+    	if(SwapMetrics::_overutilized){
     		sleep(1);
+    		continue;
+    		// acquire a lock here
     	}//while(SwapMetrics::_overutilized);
     if (!_work_queue->pop_local(new_oop)) {
       // We emptied our work_queue; check if there's stuff that can
