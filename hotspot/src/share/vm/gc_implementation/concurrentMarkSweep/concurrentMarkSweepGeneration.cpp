b579d80a8d9d57b1c89f88cc9c9d67ad261eff69
ravitandon <ravifreek63@gmail.com>
2014-11-20 18:09:47 -0500

    debugging continuing workers

--- hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
+++ hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
@@ -4308,6 +4308,7 @@ bool CMSCollector::do_marking_mt(bool asynch) {
   conc_workers()->start_task(&tsk);
   while (tsk.yielded()) {
     tsk.coordinator_yield();
+    cout << "Continuing workers" << endl;
     conc_workers()->continue_task(&tsk);
   }
   // If the task was aborted, _restart_addr will be non-NULL
