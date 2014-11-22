5f4503666ff5f2657eaf72c4a6f68a49d3a1e569
ravitandon <ravifreek63@gmail.com>
2014-11-22 01:51:27 -0500

    debugging collect in background

--- hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepThread.cpp
+++ hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepThread.cpp
@@ -127,12 +127,12 @@ void ConcurrentMarkSweepThread::run() {
   while (!_should_terminate) {
     sleepBeforeNextCycle();
     if (_should_terminate) break;
-    if(Universe::isGCSignalled){
-    	cout << "Calling collect_in_background isGCSignalled =  "<< Universe::isGCSignalled << endl;
+    //if(Universe::isGCSignalled){
+    	//cout << "Calling collect_in_background isGCSignalled =  "<< Universe::isGCSignalled << endl;
     	_collector->collect_in_background(false);  // !clear_all_soft_refs
-    	Universe::isGCSignalled = false;
-    	cout << "collect_in_background done isGCSignalled =  "<< Universe::isGCSignalled << endl;
-    }
+    	//Universe::isGCSignalled = false;
+    	//cout << "collect_in_background done isGCSignalled =  "<< Universe::isGCSignalled << endl;
+    //}
   }
   assert(_should_terminate, "just checking");
   // Check that the state of any protocol for synchronization
@@ -319,7 +319,7 @@ void ConcurrentMarkSweepThread::sleepBeforeNextCycle() {
       wait_on_cms_lock(CMSWaitDuration);
     }
     // Check if we should start a CMS collection cycle
-    if (true || _collector->shouldConcurrentCollect()) {
+    if (_collector->shouldConcurrentCollect()) {
       return;
     }
     // .. collection criterion not yet met, let's go back
