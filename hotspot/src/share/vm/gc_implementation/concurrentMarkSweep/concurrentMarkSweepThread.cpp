6184661b44b633c28d911c5ea358a57aa67acfff
ravitandon <ravifreek63@gmail.com>
2014-12-14 22:16:14 -0500

    background collection is stopped

--- hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepThread.cpp
+++ hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepThread.cpp
@@ -130,7 +130,7 @@ void ConcurrentMarkSweepThread::run() {
     //if(Universe::isGCSignalled){
     	//if(_numberCollectionsLeft>0){
     		//cout << "Calling collect_in_background isGCSignalled =  "<< Universe::isGCSignalled << endl;
-//    	_collector->collect_in_background(false);  // !clear_all_soft_refs
+    	_collector->collect_in_background(false);  // !clear_all_soft_refs
     		//_numberCollectionsLeft--;
     	//}
     //}
@@ -320,7 +320,7 @@ void ConcurrentMarkSweepThread::sleepBeforeNextCycle() {
       wait_on_cms_lock(CMSWaitDuration);
     }
     // Check if we should start a CMS collection cycle
-    if (_collector->shouldConcurrentCollect()) {
+    if (false && _collector->shouldConcurrentCollect()) {
       return;
     }
     // .. collection criterion not yet met, let's go back
