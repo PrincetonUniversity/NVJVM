7bc96a9b46558efbd59c3a36fe28793785f18989
ravitandon <ravifreek63@gmail.com>
2014-12-18 00:12:38 -0500

    debugging

--- hotspot/src/share/vm/gc_implementation/parallelScavenge/psParallelCompact.cpp
+++ hotspot/src/share/vm/gc_implementation/parallelScavenge/psParallelCompact.cpp
@@ -3712,7 +3712,6 @@ void PSParallelMarkingTask::masterMarkingTask(){
 			}
 			_partitionMetaData->setToWork();
 			usleep(1000);
-//			cout << "Grey object count in master = " << _partitionMetaData->getTotalGreyObjectsChunkLevel() << endl;
 		}
 		printf("Master has come to an end.\n");
 }
