a2b17e566653bf1bc2a49b273f6cf618cc76eaea
ravitandon <ravifreek63@gmail.com>
2014-12-23 18:59:03 -0500

    removed pressure

--- hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
+++ hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
@@ -2297,7 +2297,7 @@ void CMSCollector::collect_in_foreground(bool clear_all_soft_refs) {
 #if TRACING_SYSTEM_GC
 	printf("TRACING_SYSTEM_GC:: in collect_in_foreground");
 #endif
-	MemPressureStats::signalPressure();
+	//MemPressureStats::signalPressure();
   assert(_foregroundGCIsActive && !_foregroundGCShouldWait,
          "Foreground collector should be waiting, not executing");
   assert(Thread::current()->is_VM_thread(), "A foreground collection"
@@ -2399,7 +2399,7 @@ void CMSCollector::collect_in_foreground(bool clear_all_soft_refs) {
         Thread::current(), _collectorState);
     }
   }
-  MemPressureStats::signalReleasePressure();
+//  MemPressureStats::signalReleasePressure();
   if (UseAdaptiveSizePolicy) {
     GenCollectedHeap* gch = GenCollectedHeap::heap();
     size_policy()->ms_collection_end(gch->gc_cause());
