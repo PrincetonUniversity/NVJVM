49bd1d48be018099c0fb1ffd02615c13cef5435e
ravitandon <ravifreek63@gmail.com>
2015-05-06 15:44:50 -0400

    removed the sweep phase

--- hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
+++ hotspot/src/share/vm/gc_implementation/concurrentMarkSweep/concurrentMarkSweepGeneration.cpp
@@ -6072,14 +6072,14 @@ void CMSCollector::sweep(bool asynch) {
     {
       CMSTokenSyncWithLocks ts(true, _cmsGen->freelistLock(),
                                bitMapLock());
-      sweepWork(_cmsGen, asynch);
+      //sweepWork(_cmsGen, asynch);
     }
 
     // Now repeat for perm gen
     if (should_unload_classes()) {
       CMSTokenSyncWithLocks ts(true, _permGen->freelistLock(),
                              bitMapLock());
-      sweepWork(_permGen, asynch);
+      //sweepWork(_permGen, asynch);
     }
 
     // Update Universe::_heap_*_at_gc figures.
